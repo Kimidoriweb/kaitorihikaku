7e7794b74daec4ab8c7f40a42ff8de90990f0f33
tamon01 <webservicecreator0421@gmail.com>
2024-05-29 18:46:03 +0900

    0.3.1

--- render-build.sh
+++ render-build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# Install Chrome
+# Set the directory where Chrome will be installed
+CHROME_DIR="$HOME/chrome"
+CHROMEDRIVER_DIR="$HOME/chromedriver"
+
+# Create directories
+mkdir -p $CHROME_DIR $CHROMEDRIVER_DIR
+
+# Download and install Chrome
 if [ $RENDER ]; then
     echo "Installing Chrome for Render.com"
-    mkdir -p /usr/local/share/chrome
-    wget -q -O /usr/local/share/chrome/chrome-linux.zip https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    unzip -q /usr/local/share/chrome/chrome-linux.zip -d /usr/local/share/chrome/
-    ln -s /usr/local/share/chrome/google-chrome /usr/bin/google-chrome
+    wget -q -O $CHROME_DIR/chrome-linux.zip https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    unzip -q $CHROME_DIR/chrome-linux.zip -d $CHROME_DIR
+    ln -s $CHROME_DIR/google-chrome /usr/local/bin/google-chrome
 fi
 
-# Install ChromeDriver
+# Download and install ChromeDriver
 if [ $RENDER ]; then
     echo "Installing ChromeDriver for Render.com"
     CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
     CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-    wget -N "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-    unzip chromedriver_linux64.zip -d /usr/local/bin/
-    rm chromedriver_linux64.zip
-    chmod +x /usr/local/bin/chromedriver
+    wget -N "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip" -P $CHROMEDRIVER_DIR
+    unzip $CHROMEDRIVER_DIR/chromedriver_linux64.zip -d $CHROMEDRIVER_DIR
+    ln -s $CHROMEDRIVER_DIR/chromedriver /usr/local/bin/chromedriver
+    chmod +x $CHROMEDRIVER_DIR/chromedriver
 fi
